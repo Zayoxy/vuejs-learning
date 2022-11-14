35530ac452ed3c9055f1de42ca8037cd4f21a1a9
Alexandre <alexandreguillaume99@gmail.com>
2022-11-15 00:05:46 +0100

    Testing

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
-const product = 'Socks'
+const app = Vue.createApp({
+    data() {
+        return {
+            products: 'Socks',
+            description: 'Theses socks seems beautiful!'
+        }
+    }
+})
