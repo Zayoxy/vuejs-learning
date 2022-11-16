fb00bf36c5d358648ce235509ee97c0d266ce80e
Alexandre <alexandreguillaume99@gmail.com>
2022-11-16 18:04:11 +0100

    Remove button added

--- main.js
+++ main.js
@@ -1,9 +1,26 @@
 const app = Vue.createApp({
-    data() {
-        return {
-            cart: 0,
-            premium: true
-        }
+  data() {
+    return {
+      cart: [],
+      premium: true,
+    };
+  },
+  methods: {
+    updateCart(id, action) {
+      switch (action) {
+        case "add":
+          this.cart.push(id);
+          break;
+        case "remove":
+          this.cart.pop(id);
+          break;
+
+        default:
+          alert("An error has occured when updating the cart");
+          break;
+      }
+
+      console.log(this.cart);
     },
-    methods: {}
-})
+  },
+});
